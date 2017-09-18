a083ab9e2444ee467131748f94bd4122c9f8181b
Andrew <andreiduffy@gmail.com>
2017-09-18 09:17:16 +0300

    переделан вид каталога

--- includes/socials.php
+++ includes/socials.php
@@ -1,2 +1,2 @@
-<a href="#" target="_blank" rel="nofollow"><img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/icon-soc-vk.svg" alt="vk"></a>
-<a href="#" target="_blank" rel="nofollow"><img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/icon-soc-insta.svg" alt="instagram"></a>
+<a href="https://vk.com/mirtash_kzn" target="_blank" rel="nofollow"><img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/icon-soc-vk.svg" alt="vk"></a>
+<a href="https://www.instagram.com/mirtash_kzn/" target="_blank" rel="nofollow"><img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/icon-soc-insta.svg" alt="instagram"></a>

0afa8991db3429dfdeb03d83c336335b3d62ea51
andreiduffy@gmail.com <agmegadeth36>
2019-06-22 21:06:03 +0300

    1.0

---
+++ local/templates/mirtash/components/bitrix/news.list/buy_with/template.php
@@ -0,0 +1,44 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+
+<? if (!empty($arResult["ITEMS"])) : ?>
+	<section class="catalog-inner-sec">
+		<div class="wrapper">
+			<div class="catalog-inner">
+				<div class="catalog-inner__part">
+					<h2 class="catalog-inner__part-title">С этим товаром покупают</h2>
+					<div class="catalog-inner__items">
+
+						<? foreach ($arResult["ITEMS"] as $arItem): ?>
+							<?
+							$APPLICATION->IncludeComponent(
+								'bitrix:catalog.item',
+								'product_preview',
+								array(
+									'RESULT' => $arItem
+								),
+								$component,
+								array('HIDE_ICONS' => 'Y')
+							);
+							?>
+						<? endforeach; ?>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<? endif; ?>

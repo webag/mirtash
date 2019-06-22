658aff429d633550d72b83828ff7989f236e9cdb
andreiduffy@gmail.com <agmegadeth36>
2019-06-22 20:50:15 +0300

    1.0

--- local/templates/mirtash/components/bitrix/catalog.item/product_preview/template.php
+++ local/templates/mirtash/components/bitrix/catalog.item/product_preview/template.php
@@ -9,15 +9,15 @@ $this->setFrameMode(true);
 		$img = CFile::ResizeImageGet($arResult["PREVIEW_PICTURE"], array("width" => 250, "height" => 190), BX_RESIZE_IMAGE_EXACT, true);
 		?>
 		<span class="catalog-inner__item-img">
-			<img src="<?= $img['src'] ?>" alt="<?= $arResult['NAME'] ?>">
-		</span>
+		<img src="<?= $img['src'] ?>" alt="<?= $arResult['NAME'] ?>">
+	</span>
 	<? endif; ?>
 	<span class="catalog-inner__item-title"><?= $arResult['NAME'] ?></span>
 	<? if (!empty($arItem['PROPERTIES']['PRICE']['VALUE'])) : ?>
 		<span class="catalog-inner__item-price">
-			<img src="<?= SITE_TEMPLATE_PATH ?>/img/svg/icons/price.svg" alt="">
-			<?= $arItem['PROPERTIES']['PRICE']['VALUE']; ?> &#8381;
-		 </span>
+		<img src="<?= SITE_TEMPLATE_PATH ?>/img/svg/icons/price.svg" alt="">
+		<?= $arItem['PROPERTIES']['PRICE']['VALUE']; ?> &#8381;
+	 </span>
 	<? endif; ?>
 </a>
 

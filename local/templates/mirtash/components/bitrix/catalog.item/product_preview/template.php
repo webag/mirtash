2f0ab43454ff65beb64cb224130e0335f66f7a96
andreiduffy@gmail.com <agmegadeth36>
2019-06-23 10:18:23 +0300

    1.3

--- local/templates/mirtash/components/bitrix/catalog.item/product_preview/template.php
+++ local/templates/mirtash/components/bitrix/catalog.item/product_preview/template.php
@@ -3,20 +3,40 @@
 $this->setFrameMode(true);
 ?>
 
+<?
+$item_class = "";
+switch ($arResult['PROPERTIES']['LABLE']['VALUE_XML_ID']){
+	case '1':
+		$item_class = "catalog-inner__item--pink";
+		break;
+	case '2':
+		$item_class = "catalog-inner__item--blue";
+		break;
+	case '3':
+		$item_class = "catalog-inner__item--yellow";
+		break;
+}
+?>
 
-<a href="<?= $arResult['DETAIL_PAGE_URL'] ?>" class="catalog-inner__item">
-	<? if (!empty($arResult["PREVIEW_PICTURE"]["SRC"])) : ?>
+<a href="<?= $arResult['DETAIL_PAGE_URL'] ?>" class="catalog-inner__item <?=$item_class?>">
+
+	<div class="catalog-inner__item-img">
+		<? if (!empty($arResult["PREVIEW_PICTURE"]["SRC"])) : ?>
 		<?
 		$img = CFile::ResizeImageGet($arResult["PREVIEW_PICTURE"], array("width" => 250, "height" => 190), BX_RESIZE_IMAGE_EXACT, true);
 		?>
-		<span class="catalog-inner__item-img">
-			<img src="<?= $img['src'] ?>" alt="<?= $arResult['NAME'] ?>">
-		</span>
-	<? endif; ?>
-	<span class="catalog-inner__item-title"><?= $arResult['NAME'] ?></span>
+		<img src="<?= $img['src'] ?>" alt="<?= $arResult['NAME'] ?>">
+		<? endif; ?>
+		<? if (!empty($arResult['PROPERTIES']['LABLE']['VALUE'])) : ?>
+			<div class="catalog-inner__item-tag"><?=$arResult['PROPERTIES']['LABLE']['VALUE']?></div>
+		<? endif; ?>
+	</div>
+
+	<div class="catalog-inner__item-title"><?= $arResult['NAME'] ?></div>
+
 	<? if (!empty($arResult['PROPERTIES']['PRICE']['VALUE'])) : ?>
 		<span class="catalog-inner__item-price">
-			<img src="<?= SITE_TEMPLATE_PATH ?>/img/svg/icons/price.svg" alt="">
+			<img src="<?=SITE_TEMPLATE_PATH ?>/img/svg/icons/price.svg" alt="">
 			<?= $arResult['PROPERTIES']['PRICE']['VALUE']; ?> &#8381;
 		 </span>
 	<? endif; ?>
--- local/templates/mirtash/components/bitrix/catalog/catalog/bitrix/catalog.section/.default/template.php
+++ local/templates/mirtash/components/bitrix/catalog/catalog/bitrix/catalog.section/.default/template.php
@@ -21,36 +21,16 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 					<div class="catalog-inner__items">
 						<? foreach($arSection["ELEMENTS"] as $arElement): ?>
 							<?
-							$item_class = "";
-							switch ($arElement['PROPERTIES']['LABLE']['VALUE_XML_ID']){
-								case '1':
-									$item_class = "catalog-inner__item--pink";
-									break;
-								case '2':
-									$item_class = "catalog-inner__item--blue";
-									break;
-								case '3':
-									$item_class = "catalog-inner__item--yellow";
-									break;
-							}
+							$APPLICATION->IncludeComponent(
+								'bitrix:catalog.item',
+								'product_preview',
+								array(
+									'RESULT' => $arElement
+								),
+								$component,
+								array('HIDE_ICONS' => 'Y')
+							);
 							?>
-							<a href="<?=$arElement["DETAIL_PAGE_URL"];?>" class="catalog-inner__item <?=$item_class?>">
-						<span class="catalog-inner__item-img">
-						<? if (!empty($arElement["PREVIEW_PICTURE"]["SRC"])) : ?>
-							<?
-							$img = CFile::ResizeImageGet($arElement["PREVIEW_PICTURE"], array("width" => 250, "height" => 190), BX_RESIZE_IMAGE_EXACT, true);
-							?>
-							<img src="<?=$img['src']?>" alt="<?=$arElement['NAME']?>">
-						<? endif; ?>
-							<? if (!empty($arElement['PROPERTIES']['LABLE']['VALUE'])) : ?>
-								<span class="catalog-inner__item-tag"><?=$arElement['PROPERTIES']['LABLE']['VALUE']?></span>
-							<? endif; ?>
-						</span>
-								<span class="catalog-inner__item-title"><?=$arElement["NAME"];?></span>
-								<? if (!empty($arElement['PROPERTIES']['PRICE']['VALUE'])) : ?>
-									<span class="catalog-inner__item-price"><img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/price.svg" alt=""><?=$arElement['PROPERTIES']['PRICE']['VALUE'];?> &#8381;</span>
-								<? endif; ?>
-							</a>
 						<?endforeach?>
 					</div>
 				</div>

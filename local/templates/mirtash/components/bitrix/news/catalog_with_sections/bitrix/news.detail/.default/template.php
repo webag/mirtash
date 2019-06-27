272b40c135f7b18fc4dba82db3a26ed835cbdb5f
Andrew <andreiduffy@gmail.com>
2019-06-27 10:21:08 +0300

    1.5 - targets

--- local/templates/mirtash/components/bitrix/news/catalog_with_sections/bitrix/news.detail/.default/template.php
+++ local/templates/mirtash/components/bitrix/news/catalog_with_sections/bitrix/news.detail/.default/template.php
@@ -274,7 +274,7 @@ $arrFilter = Array(
 						<h3>Оставьте ваши контактные данные</h3>
 						<p>Специалист свяжется с вами в ближайшее время</p>
 						<form class="ajax-form">
-							<input type="hidden" value="callback" name="target_id">
+							<input type="hidden" value="consult" name="target_id">
 							<input type="text" class="input-text" name="user_name" placeholder="Ваше имя" data-label="Имя пользователя">
 							<input type="tel" class="input-text" name="user_phone" placeholder="Номер телефона*" data-label="Телефон" data-req="true">
 							<input type="email" class="input-text" name="user_email" placeholder="E-mail*" data-label="E-mail" data-req="true">
